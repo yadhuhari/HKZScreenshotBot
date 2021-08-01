667cd9af663dfaeb976e0401e9b18ae9d07ff4d0
naveen9329 <69340807+naveen9329@users.noreply.github.com>
2021-08-01 11:56:48 +0530

    Update base.py

--- bot/processes/base.py
+++ bot/processes/base.py
@@ -1,3 +1,4 @@
+import asyncio
 from abc import ABC, abstractmethod
 
 from bot.config import Config
@@ -15,10 +16,11 @@ class BaseProcess(ABC):
         self._media_message = None
 
     @property
-    async def file_link(self):
+    def file_link(self):
         if self._file_link is None:
             if self.media_message.media:
-                self._file_link = await Utilities.generate_stream_link(self.media_message)
+                self._file_link = asyncio.create_task(Utilities.generate_stream_link(self.media_message))
+                asyncio.ensure_future(Utilities.generate_stream_link(self.media_message))
             else:
                 self._file_link = self.media_message.text
         return self._file_link
