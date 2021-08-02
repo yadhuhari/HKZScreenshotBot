97ddda8eff45d041aeafb375cb83af85992e250f
naveen9329 <69340807+naveen9329@users.noreply.github.com>
2021-08-02 11:37:52 +0530

    Update utils.py

--- bot/utils/utils.py
+++ bot/utils/utils.py
@@ -345,7 +345,7 @@ class Utilities:
                 i_keyboard = []
             if i == 10:
                 btns.append(i_keyboard)
-        btns.append([InlineKeyboardButton("Manual Screenshots!", "mscht")])
-        btns.append([InlineKeyboardButton("Trim Video!", "trim")])
-        btns.append([InlineKeyboardButton("Get Media Information", "mi")])
+        btns.append([InlineKeyboardButton("Manual Screenshots 🦋", "mscht")])
+        btns.append([InlineKeyboardButton("Trim Video 💢", "trim")])
+        btns.append([InlineKeyboardButton("Get Media Information 📋", "mi")])
         return btns
