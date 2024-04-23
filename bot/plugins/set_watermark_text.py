87d521149078909c2f53fe453c8016728efe41cb
GOD OF THUNDER <93386525+yadhuhari@users.noreply.github.com>
2024-04-23 18:47:51 +0530

    Update set_watermark_text.py

--- bot/plugins/set_watermark_text.py
+++ bot/plugins/set_watermark_text.py
@@ -12,8 +12,10 @@ async def _(c, m):
 
     if len(m.command) == 1:
         await m.reply_text(
-            text="You can add custom watermark text to the screenshots.\n\nUsage: `/set_watermark text`. "
-            "Text should not Exceed 30 characters.",
+            text="""Yᴏᴜ ᴄᴀɴ ᴀᴅᴅ ᴄᴜsᴛᴏᴍ ᴡᴀᴛᴇʀᴍᴀʀᴋ ᴛᴇxᴛ ᴛᴏ ᴛʜᴇ sᴄʀᴇᴇɴsʜᴏᴛs.
+
+Usᴀɢᴇ: /set_watermark ᴛᴇxᴛ.
+Tᴇxᴛ sʜᴏᴜʟᴅ ɴᴏᴛ Exᴄᴇᴇᴅ 𝟹𝟶 ᴄʜᴀʀᴀᴄᴛᴇʀs.""",
             quote=True
         )
         return
@@ -21,15 +23,15 @@ async def _(c, m):
     watermark_text = " ".join(m.command[1:])
     if len(watermark_text) > 30:
         await m.reply_text(
-            text=f"The watermark text you provided (__{watermark_text}__) is `{len(watermark_text)}` "
-            "characters long! You cannot set watermark text greater than 30 characters.",
+            text=f"Tʜᴇ ᴡᴀᴛᴇʀᴍᴀʀᴋ ᴛᴇxᴛ ʏᴏᴜ ᴘʀᴏᴠɪᴅᴇᴅ (__{watermark_text}__) ɪs `{len(watermark_text)}` "
+            "ᴄʜᴀʀᴀᴄᴛᴇʀs ʟᴏɴɢ! Yᴏᴜ ᴄᴀɴɴᴏᴛ sᴇᴛ ᴡᴀᴛᴇʀᴍᴀʀᴋ ᴛᴇxᴛ ɢʀᴇᴀᴛᴇʀ ᴛʜᴀɴ 𝟹𝟶 ᴄʜᴀʀᴀᴄᴛᴇʀs.",
             quote=True
         )
         return
 
     await db.update_watermark_text(m.chat.id, watermark_text)
     await m.reply_text(
-        text=f"You have successfully set __{watermark_text}__ as your watermark text. From now on this will "
-        "be applied to your screenshots! To remove watermark text see /settings.",
+        text=f"Yᴏᴜ ʜᴀᴠᴇ sᴜᴄᴄᴇssғᴜʟʟʏ sᴇᴛ __{watermark_text}__ ᴀs ᴡᴀᴛᴇʀᴍᴀʀᴋ ᴛᴇxᴛ. Fʀᴏᴍ ɴᴏᴡ ᴏɴ ᴛʜɪs ᴡɪʟʟ "
+        "ʙᴇ ᴀᴘᴘʟɪᴇᴅ ᴛᴏ ʏᴏᴜʀ sᴄʀᴇᴇɴsʜᴏᴛs! Tᴏ ʀᴇᴍᴏᴠᴇ ᴡᴀᴛᴇʀᴍᴀʀᴋ ᴛᴇxᴛ sᴇᴇ /sᴇᴛᴛɪɴɢs.",
         quote=True
     )
